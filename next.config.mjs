4c743c12d978c9ab20126c32b3b60921e1b70c98
Adedamola Adedapo <adedadamola@gmail.com>
2025-05-29 13:05:29 +0100

    Update next.config.mjs
    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    // Add alias for @ to point to ./src
+    config.resolve.alias['@'] = path.resolve('./src');
+    return config;
+  }
+};
 
 export default nextConfig;
+
