3e3a79af21a71e20800c847de4c1a179d2b95a57
Williams Alex <godswillwilliams9@gmail.com>
2025-07-07 11:02:45 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
+
 function LoginPage() {
-  return <main>Uburu Scanner</main>;
+  return (
+    <main>
+      <div className="w-full h-screen flex justify-center items-center">
+        <div>
+          <div className="flex justify-center">
+            <div className="relative w-[50px] h-[50px]">
+              <Image
+                alt=""
+                src={"/images/init-scan.png"}
+                fill
+                className="absolute"
+              />
+            </div>
+          </div>
+
+          <p className="text-center">Coming soon</p>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default LoginPage;
